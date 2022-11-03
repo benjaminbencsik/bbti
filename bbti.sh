80929e641e6e444fc0f0a79bb8d5b17c3b4f6525
benjaminbencsik <116736668+benjaminbencsik@users.noreply.github.com>
2022-11-03 14:57:04 -0400

    Update bbti.sh

--- bbti.sh
+++ bbti.sh
@@ -25,19 +25,67 @@ echo "Finished installing dependacies"
 echo "Starting tool installation" 
 
 #installing tools 
-go install -v github.com/OWASP/Amass/v3/...@master
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
-go install    github.com/projectdiscovery/asnmap/cmd/asnmap@latest
-go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest
-go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
-go install    github.com/tomnomnom/assetfinder@latest
-go install    github.com/tomnomnom/waybackurls@latest
-go install    github.com/tomnomnom/meg@latest
-go install    github.com/tomnomnom/gron@latest
-go install    github.com/lc/gau/v2/cmd/gau@latest
+
+echo "Installing golang tools please wait..."
+go install -v github.com/OWASP/Amass/v3/...@master > /dev/null 2>&1
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest > /dev/null 2>&1
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest > /dev/null 2>&1
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest > /dev/null 2>&1
+go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest > /dev/null 2>&1 
+go install    github.com/projectdiscovery/asnmap/cmd/asnmap@latest > /dev/null 2>&1
+go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest > /dev/null 2>&1
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest > /dev/null 2>&1
+go install    github.com/tomnomnom/assetfinder@latest > /dev/null 2>&1
+go install    github.com/tomnomnom/waybackurls@latest > /dev/null 2>&1
+go install    github.com/tomnomnom/meg@latest > /dev/null 2>&1
+go install    github.com/tomnomnom/gron@latest > /dev/null 2>&1
+go install    github.com/lc/gau/v2/cmd/gau@latest > /dev/null 2>&1
+echo "Finished installing golang tools" 
+
+echo "Istalling other tools"
+cd ~/tools 
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r 
+sudo pip install -r requirements.txt > /dev/null 2>&1
+sudo apt-get install python-requests > /dev/null 2>&1
+sudo apt-get install python-argparse > /dev/null 2>&1
+cd ..
+git clone https://github.com/christophetd/censys-subdomain-finder.git > /dev/null 2>&1
+cd censys-subdomain-finder > /dev/null 2>&1
+python3 -m venv venv > /dev/null 2>&1
+source venv/bin/activate > /dev/null 2>&1
+pip install -r requirements.txt > /dev/null 2>&1
+cd ..
+git clone --recursive https://github.com/screetsec/Sudomy.git > /dev/null 2>&1
+python3 -m pip install -r requirements.txt > /dev/null 2>&1
+apt-get install jq nmap phantomjs npm chromium parallel > /dev/null 2>&1
+npm i -g wappalyzer wscat > /dev/null 2>&1
+
+git clone https://github.com/jonluca/Anubis > /dev/null 2>&1
+cd Anubis > /dev/null 2>&1
+pip3 install -r requirments.txt > /dev/null 2>&1
+pip3 install .  > /dev/null 2>&1 
+git clone https://github.com/edoardottt/scilla.git
+
+
+
+
+
+
+
+echo "Finished installing other tools" 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
